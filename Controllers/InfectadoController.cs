f9b2e63e19dfdd636454b572839c31a6c3b918a1
Khayan Malantrucco <khayanmalantrucco@gmail.com>
2021-01-11 22:21:00 -0300

    Curso API C# + MongoDB

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,61 @@
+using System;
+using Api.Data.Collections;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso!");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+
+        [HttpPut]
+        public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter
+                .Where(x => x.DataNascimento == dto.DataNascimento),
+                Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+
+            return Ok("Infectado atualizado com sucesso!");
+        }
+
+        [HttpDelete("{dataNascimento}")]
+        public ActionResult RemoverInfectado(DateTime dataNascimento)
+        {
+            _infectadosCollection.DeleteOne(Builders<Infectado>.Filter
+                .Where(x => x.DataNascimento == dataNascimento));
+
+            return Ok("Infectado removido com sucesso!");
+        }
+    }
+}
